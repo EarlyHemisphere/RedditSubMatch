6343f424f2fe532287eb81968c23cc9a73408bdd
Lucas Anderson <lucasanderson1999@gmail.com>
2020-02-03 18:51:39 -0500

    updated saving to db

--- functions/src/helpers.ts
+++ functions/src/helpers.ts
@@ -27,26 +27,8 @@ export const getAccessToken = async (code: string, clientid: string, secret: str
     if (data.error) {
         throw `Error: ${data.error}`
     } 
-    return { access_token: data['access_token'], refresh_token: data['refresh_token']}
+    return { accessToken: data['access_token'], refreshToken: data['refresh_token']}
 }
-
-
-const subredditWhiteListKeys = ["display_name", "user_is_banned", "subscribers", "user_is_contributor", "created_utc", "lang"]
-
-const filterKeys = (obj: any, keys: string[]) => {
-    const filtered: any = {}
-    keys.forEach(key => {
-      if (obj.hasOwnProperty(key)) {
-        filtered[key] = obj[key]
-      }
-    })
-    return filtered
-}
-
-export const formatSubreddits = (subreddits: any) => {
-    return subreddits.map((sub: any)=>filterKeys(sub.data, subredditWhiteListKeys))
-}
-
 
 export const getUserInfo = async (accessToken: string) => {
     let uri = `https://oauth.reddit.com/api/v1/me`
@@ -60,34 +42,4 @@ export const getUserInfo = async (accessToken: string) => {
     })
 
     return response
-}
-
-
-export const getSubredditInfo = async (accessToken: string, after?:string) => {
-    let uri = `https://oauth.reddit.com/subreddits/mine/subscriber?limit=100`
-    if(after){
-        uri = `https://oauth.reddit.com/subreddits/mine/subscriber?limit=100&after=${after}`
-    }
-    let response = await rp({
-        uri: uri,
-        json: true,
-        headers: {
-            Authorization: `bearer ${accessToken}`,
-            'User-Agent': 'Submatch/0.1 by Submatch_bot'
-        }
-    })
-
-    return response
-}
-
-export const getSubreddits = async (accessToken: string) => {
-    let response = await getSubredditInfo(accessToken) 
-    let subreddits = response.data.children
-    while (response.data.after !== null) {
-        console.log(response.data.after)
-        response = await getSubredditInfo(accessToken, response.data.after)
-        console.log(response.data.after)
-        subreddits = [...subreddits, ...response.data.children]
-    }
-    return subreddits
 }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,28 +2,23 @@ import * as functions from 'firebase-functions';
 import { getAccessToken, /*getSubreddits, formatSubreddits,*/ getUserInfo } from './helpers';
 import * as admin from 'firebase-admin';
 
-
-
 admin.initializeApp()
 const firestore = admin.firestore()
-
 
 interface submitUserLogin_i {
     code: string
 }
 
-
-exports.submitUserLogin = functions.https.onCall(async (data: submitUserLogin_i, context) => {
+exports.submitUserLogin = functions.https.onCall(async (data: submitUserLogin_i) => {
     return new Promise(async (res, rej) => {
         let accessToken
         let refreshToken
-        // let subreddits
         let userInfo
         let USERNAME
 
         console.log("GETTING ACCESS TOKEN AND REFRESH TOKEN")
         try {
-            ({ refresh_token: refreshToken, access_token: accessToken } = await getAccessToken(data.code, functions.config().reddit.clientid, functions.config().reddit.secret))
+            ({ refreshToken, accessToken } = await getAccessToken(data.code, functions.config().reddit.clientid, functions.config().reddit.secret))
         } catch(err) {
             console.error("FAILED GETTING ACCESS TOKEN AND REFRESH TOKEN", err)
             res({ ok: false, message: "error getting access token" })
@@ -31,6 +26,7 @@ exports.submitUserLogin = functions.https.onCall(async (data: submitUserLogin_i,
         }
         console.log(accessToken)
         console.log(refreshToken)
+
         console.log("GETTING USER INFO")
         try {
             userInfo = await getUserInfo(accessToken)
@@ -40,29 +36,13 @@ exports.submitUserLogin = functions.https.onCall(async (data: submitUserLogin_i,
             return
         }
 
-        // console.log("GETTING SUBREDDITS")
-        // try {
-        //     subreddits = await getSubreddits(accessToken)
-        // } catch(err) {
-        //     console.error("FAILED GETTING SUBREDDITS", err)
-        //     res({ ok: false, message: "error getting subreddits" })
-        //     return
-        // }
-
         USERNAME = userInfo.name
         console.log("ADDING TO FIRESTORE DB")
-        // take the list of subreddits and add it to a database
         await firestore.collection("users").doc(USERNAME).set({
             timestamp: new Date().getTime(),
-            accessToken,
             refreshToken,
-            //subreddits: formatSubreddits(subreddits)
         })
         
         res({ ok: true, message: "success" })
     })
 })
-
-
-
-// exports.match = functions.pubsub
