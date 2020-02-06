41d407218806ae78275170a6d4fa32e13a00a254
Lucas Anderson <lucasanderson1999@gmail.com>
2020-02-05 22:08:24 -0500

    more info in success pages

--- frontend/src/app/containers/Success/index.tsx
+++ frontend/src/app/containers/Success/index.tsx
@@ -29,10 +29,22 @@ export default function success(props) {
             <Typography variant="h4" display="block" gutterBottom classes={{ root: style.text }}>
               You have been <b style={{ color: '#29a329' }}>removed</b> from matching
             </Typography>
-            <Typography variant="h5" display="block" gutterBottom classes={{ root: style.text }}>and your data has been deleted.</Typography>
+            <Typography variant="h5" display="block" classes={{ root: style.text }} style={{ marginBottom: '4rem' }}>and your data has been deleted.</Typography>
+            <Typography variant="h5" display="block" gutterBottom classes={{ root: style.text }}>
+              You can always <a href='https://wwww.reddit-submatch.web.app' style={{ color: 'yellow' }}>opt in</a>
+            </Typography>
+            <Typography variant="h5" display="block" gutterBottom classes={{ root: style.text }}>
+              or visit <a href='https://www.reddit.com/r/submatch' style={{ color: '#FF4500' }}>the subreddit</a> for updates about matching and community events.
+            </Typography>
           </>
         ) : (
-          <Typography variant="h4" display="block" gutterBottom classes={{ root: style.text }}>success</Typography>
+          <>
+            <Typography variant="h2" display="block" classes={{ root: style.text }} style={{ color: '#29a329', marginBottom: '6rem' }}><b>Success!</b></Typography>
+            <Typography variant="h4" display="block" classes={{ root: style.text }} style={{ marginBottom: '3rem' }}>You are now participating in regular matching!</Typography>
+            <Typography variant="h5" display="block" gutterBottom classes={{ root: style.text }}>For more info or if you have any questions or concerns,</Typography>
+            <Typography variant="h5" display="block" gutterBottom classes={{ root: style.text }}>
+              head over to <a href='https://www.reddit.com/r/submatch' style={{ color: '#FF4500' }}>r/submatch</a>.</Typography>
+          </>
         )}
       </Grid>
     </Container>
