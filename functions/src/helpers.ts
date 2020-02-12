ce751cc56872b7ef3d5234c92280b58e11d97c40
mframe <mframe45@gmail.com>
2020-02-12 14:41:06 -0500

    updated the healpers to include refresh token shiz

--- functions/src/helpers.ts
+++ functions/src/helpers.ts
@@ -62,6 +62,24 @@ export const getUserInfo = async (accessToken: string) => {
     return response
 }
 
+export const useRefreshToken = async (refreshToken: string, clientid: string, secret: string) => {
+    let uri = `https://www.reddit.com/api/v1/access_token?grant_type=refresh_token&refresh_token=${refreshToken}`
+    console.log(uri)
+    let response = await rp({
+        uri: uri,
+        method: 'POST',
+        headers: {
+            // Authorization: `Basic ${btoa(clientid + ":" + secret)}`,
+            'User-Agent': 'Submatch/0.1 by Submatch_bot'
+        },
+        json: true,
+    }).auth(clientid, secret)
+    if (response.error === 400) {
+        throw `Error: ${response.message}`
+    } 
+    return response.access_token
+}
+
 
 export const getSubredditInfo = async (accessToken: string, after?:string) => {
     let uri = `https://oauth.reddit.com/subreddits/mine/subscriber?limit=100`
