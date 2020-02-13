6f65380ca02f1a567117ad8955b24294b6eebef1
Lucas Anderson <lucasanderson1999@gmail.com>
2020-02-13 15:07:08 -0500

    error page ui

--- frontend/src/app/containers/Success/Error/index.tsx
+++ frontend/src/app/containers/Success/Error/index.tsx
@@ -1,9 +1,27 @@
 import * as React from 'react'
+import * as style from '../style.scss';
+import { Container, Grid, Typography } from '@material-ui/core';
 
 export const Error = (response) => {
     return (
-        <div>
-            Error
-        </div>
+        <Container maxWidth={false} classes={{ root: style.normal }}>
+        	<Grid container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justify="flex-start"
+                style={{ minHeight: '100vh' }}>
+                <Typography variant="h3" display="block" classes={{ root: style.title }}>submatch</Typography>
+                <Grid
+                	direction="row"
+                	alignItems="flex-start"
+                	justify="flex-start"
+                	style={{ height: '200px', width: '40%'}}>
+                	<Typography variant="h4" display="block">API Error:</Typography>
+                	<Typography variant="h5" display="block" style={{ marginBottom: 100}}>{response.message}</Typography>
+                	<Typography variant="h4" display="block">Please try again later.</Typography>
+                </Grid>
+            </Grid>
+        </Container>
     )
 }
