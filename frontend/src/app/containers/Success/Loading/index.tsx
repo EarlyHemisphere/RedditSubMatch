b719dd1bd74a0714cbae3fb4e5419a95a482149b
Lucas Anderson <lucasanderson1999@gmail.com>
2020-02-13 13:05:42 -0500

    loading page UI

--- frontend/src/app/containers/Success/Loading/index.tsx
+++ frontend/src/app/containers/Success/Loading/index.tsx
@@ -1,9 +1,20 @@
 import * as React from 'react'
+import * as style from '../style.scss';
+import { Container, Grid, Typography, Backdrop, CircularProgress } from '@material-ui/core';
 
-export const Loading = () => {
+export const Loading = (props) => {
     return (
-        <div>
-            Loading
-        </div>
+        <Container maxWidth={false} classes={{ root: style.normal }}>
+        	<Grid container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                style={{ minHeight: '100vh' }}>
+                <Typography variant="h3" display="block" classes={{ root: style.title }}>submatch</Typography>
+        		<Backdrop open={true} style={{ zIndex: 1 }}>
+        			<CircularProgress color="inherit" />
+      			</Backdrop>
+      		</Grid>
+        </Container>
     )
 }
